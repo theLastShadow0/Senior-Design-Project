996b93317ad370d19260f7b72f9359a1e6ae1151
Andres Lopez <163787540+theLastShadow0@users.noreply.github.com>
2025-03-12 13:49:44 -0500

    Update autoFollowIsolated.ino

--- autoFollowIsolated.ino
+++ autoFollowIsolated.ino
@@ -26,62 +26,58 @@ SoftwareSerial mySerial(11, 12);
 
 void printResult(HUSKYLENSResult result);
 
-void setup() {
-    Serial.begin(9600);
-    mySerial.begin(9600);
+void setup(){
+  Serial.begin(9600);
+  mySerial.begin(9600);
 
-    pinMode (A0, OUTPUT);
-    pinMode (A1, OUTPUT);
-    pinMode (A2, OUTPUT);
-    pinMode (A3, OUTPUT);
-
-    
-
-int retryCount = 0;
-while (!huskylens.begin(mySerial) && retryCount < 10)
-{
+  pinMode (A0, OUTPUT);
+  pinMode (A1, OUTPUT);
+  pinMode (A2, OUTPUT);
+  pinMode (A3, OUTPUT);
+  int retryCount = 0;
+  
+  while (!huskylens.begin(mySerial) && retryCount < 10){
     Serial.println(F("Begin failed!"));
     delay(100);
     retryCount++;
-}
-if (retryCount >= 10) {
-    Serial.println(F("HUSKYLENS initialization failed. Check wiring."));
-}
-
-}
-
-void loop() 
-{
-    if (!huskylens.request()) 
-Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
-
-    else if(!huskylens.isLearned()) 
-Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one!"));
-
-    else if(!huskylens.available()) 
-Serial.println(F("No block or arrow appears on the screen!"));
-
-    else
-    {
-        Serial.println(F("###########"));
-        while (huskylens.available())
-        {
-            HUSKYLENSResult result = huskylens.read();
-            printResult(result);
-            driveBot(result);
-        }    
     }
+    
+    if (retryCount >= 10) {
+    Serial.println(F("HUSKYLENS initialization failed. Check wiring."));
+    }
+}
+
+void loop() {
+  if (!huskylens.request()){
+    Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
+  }
+  
+  else if(!huskylens.isLearned()){
+    Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one!"));
+  }
+
+  else if(!huskylens.available()){
+    Serial.println(F("No block or arrow appears on the screen!"));
+  }
+
+  else{
+    Serial.println(F("###########"));
+    while (huskylens.available()){
+      HUSKYLENSResult result = huskylens.read();
+      printResult(result);
+      driveBot(result);
+    }    
+  }
 }
 
 
 void printResult(HUSKYLENSResult result){
-    if (result.command == COMMAND_RETURN_BLOCK){
-        Serial.print("Block: xCenter="); Serial.print(result.xCenter);
-        Serial.print(", yCenter="); Serial.print(result.yCenter);
-        Serial.print(", width="); Serial.print(result.width);
-        Serial.print(", height="); Serial.print(result.height);
-        Serial.print(", ID="); Serial.println(result.ID);
-
+  if (result.command == COMMAND_RETURN_BLOCK){
+    Serial.print("Block: xCenter="); Serial.print(result.xCenter);
+    Serial.print(", yCenter="); Serial.print(result.yCenter);
+    Serial.print(", width="); Serial.print(result.width);
+    Serial.print(", height="); Serial.print(result.height);
+    Serial.print(", ID="); Serial.println(result.ID);
     }
     else if (result.command == COMMAND_RETURN_ARROW){
     Serial.print("Arrow:Origin=");Serial.print(result.xOrigin);
@@ -90,11 +86,12 @@ void printResult(HUSKYLENSResult result){
     Serial.print(",yTarget=");Serial.print(result.yTarget);
     Serial.print(",ID=");Serial.print(result.ID);
     }
-
     else{
         Serial.println("Object unknown!");
     }
 }
+
+
 void go_Advance() {  
   digitalWrite(RightDirectPin1, HIGH);
   digitalWrite(RightDirectPin2,LOW);
@@ -102,6 +99,7 @@ void go_Advance() {
   digitalWrite(LeftDirectPin2,LOW);
   //set_Motorspeed(100,100);
    }
+
 void go_Left() { 
   //digitalWrite(RightDirectPin1, HIGH);
   //digitalWrite(RightDirectPin2,LOW);
@@ -109,13 +107,15 @@ void go_Left() {
   digitalWrite(LeftDirectPin2,HIGH);
   //set_Motorspeed(TURN_SPEED2,TURN_SPEED1);
  }
+
 void go_Right() {   
   digitalWrite(RightDirectPin1,LOW);
   digitalWrite(RightDirectPin2,HIGH);
   //digitalWrite(LeftDirectPin1,HIGH);
   //digitalWrite(LeftDirectPin2,LOW);
   //set_Motorspeed(TURN_SPEED1,TURN_SPEED2);
-   }
+  }
+
 void go_Back() {   
   digitalWrite(RightDirectPin1, LOW);
   digitalWrite(RightDirectPin2,HIGH);
@@ -123,46 +123,42 @@ void go_Back() {
   digitalWrite(LeftDirectPin2,HIGH);
   //set_Motorspeed(100,100);
   }
+
 void stop_Stop() { 
   digitalWrite(RightDirectPin1, LOW);
   digitalWrite(RightDirectPin2,LOW);
   digitalWrite(LeftDirectPin1,LOW);
   digitalWrite(LeftDirectPin2,LOW);
   set_Motorspeed(0,0);
- }
- void set_Motorspeed(int speed_L,int speed_R)
-{
+  }
+
+void set_Motorspeed(int speed_L,int speed_R){
   analogWrite(speedPinL,speed_L);
-  analogWrite(speedPinR,speed_R);  
-}
-
-
-
-void driveBot(HUSKYLENSResult result)
-{
-
-if(result.xCenter<=120)
-  {
-    go_Left();
-    set_Motorspeed(TURN_SPEED3,TURN_SPEED2);
+  analogWrite(speedPinR,speed_R);
   }
 
-else if(result.xCenter>=180)
-  {
-    go_Right();
-    set_Motorspeed(TURN_SPEED2,TURN_SPEED1);
+void driveBot(HUSKYLENSResult result){
+  if(result.xCenter<=120){
+  go_Left();
+  set_Motorspeed(TURN_SPEED3,TURN_SPEED2);
   }
-else if(result.width < 35)  // Increase threshold to allow forward movement
-{
+
+  else if(result.xCenter>=180){
+  go_Right();
+  set_Motorspeed(TURN_SPEED2,TURN_SPEED1);
+  }
+
+  else if(result.width < 35){  // Increase threshold to allow forward movement
   go_Advance();
   set_Motorspeed(SPEED, FAST_SPEED);
-}
-else if (result.width >= 35 && result.width <= 50)  // Slow down instead of stopping
-{
+  }
+
+  else if (result.width >= 35 && result.width <= 50){  // Slow down instead of stopping
   go_Advance();
   set_Motorspeed(SLOW_SPEED1, SLOW_SPEED2);
-}
-else if(result.width > 50)
-{
+  }
+
+  else if(result.width > 50){
   stop_Stop();
+  }
 }
