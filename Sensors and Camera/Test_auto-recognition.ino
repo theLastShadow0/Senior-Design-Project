10d25a2d1390ba41c83ea41fda13aaf7d8d3ff5e
eliasv-git <eliasvillarreal703@gmail.com>
2025-03-20 00:45:01 -0500

    Update Test_auto-recognition.ino

--- Sensors and Camera/Test_auto-recognition.ino
+++ Sensors and Camera/Test_auto-recognition.ino
@@ -1,36 +1,38 @@
-
 #include "HUSKYLENS.h"
 #include "SoftwareSerial.h"
 
-#define speedPinL 6  // Left PWM pin connect MODEL-X ENB
-#define speedPinR 9  // RIGHT PWM pin connect MODEL-X ENA
+#define speedPinL 6 // Left PWM pin connect MODEL-X ENB
+#define speedPinR 9 // RIGHT PWM pin connect MODEL-X ENA
 
-#define LeftDirectPin1 A2   // Left Motor direction pin 1 to MODEL-X IN3
-#define LeftDirectPin2 A3   // Left Motor direction pin 1 to MODEL-X IN4
-#define RightDirectPin1 A0  // Right Motor direction pin 1 to MODEL-X IN1
-#define RightDirectPin2 A1  // Right Motor direction pin 2 to MODEL-X IN2
+#define LeftDirectPin1 A2 // Left Motor direction pin 1 to MODEL-X IN3
+#define LeftDirectPin2 A3 // Left Motor direction pin 1 to MODEL-X IN4
+#define RightDirectPin1 A0 // Right Motor direction pin 1 to MODEL-X IN1
+#define RightDirectPin2 A1 // Right Motor direction pin 2 to MODEL-X IN2
 
-#define LeftObstacleSensor 3   // Left Infrared Sensor
-#define RightObstacleSensor 2  // Right Infrared Sensor
+#define LeftObstacleSensor 3 // Left Infrared Sensor
+#define RightObstacleSensor 2 // Right Infrared Sensor
 
-#define Echo_PIN_1 4   // Left ultrasonic sensor
-#define Trig_PIN_1 10  // Left ultrasonic sensor
-#define Echo_PIN_2 5   // Right ultrasonic sensor
-#define Trig_PIN_2 13  // Right ultrasonic sensor
+#define Echo_PIN_1 4 // Left ultrasonic sensor
+#define Trig_PIN_1 10 // Left ultrasonic sensor
+#define Echo_PIN_2 5 // Right ultrasonic sensor
+#define Trig_PIN_2 13 // Right ultrasonic sensor
 
-#define FAST_SPEED 200  //150
-#define SPEED 150       //100
-#define SLOW_SPEED1 100
-#define SLOW_SPEED2 90
+#define FAST_SPEED 200 //150
+#define SPEED 150 //100
+#define SLOW_SPEED1 90
+#define SLOW_SPEED2 80
 
-#define TURN_SPEED1 143
-#define TURN_SPEED2 53
-#define TURN_SPEED3 150
+#define TURN_SPEED1 133 //123
+#define TURN_SPEED2 63 //53
+#define TURN_SPEED3 140 //130
 
-#define BACK_SPEED1 50
+#define BACK_SPEED1 90
 #define BACK_SPEED2 80
 
 const int distancelimit = 30;
+const unsigned long AUTO_LEARN_DELAY = 15000; // 15 seconds delay
+unsigned long startTime; // To track when setup completed
+bool learningAttempted = false; // Flag to track if learning has been attempted
 
 HUSKYLENS huskylens;
 SoftwareSerial mySerial(11, 12);
@@ -74,44 +76,67 @@ void setup() {
 
   if (retryCount >= 10) {
     Serial.println(F("HUSKYLENS initialization failed. Check wiring."));
-  }
-  else {
-    // Automatically learn the object without button press
-    if (!huskylens.learnObject()) {
-      Serial.println(F("Object training failed. Please make sure an object is in front of the camera."));
-    } else {
-      Serial.println(F("Object successfully trained."));
-      
-    }
-
-    delay(2000);  // Allow time for training
-    Serial.println(F("Waiting 30 seconds before starting object recognition..."));
-    delay(30000);  // 30 seconds before starting the recognition loop
+  } else {
+    // Set algorithm to object tracking
+    huskylens.writeAlgorithm(ALGORITHM_OBJECT_TRACKING);
+    
+    // Initialize the timer for auto-learning
+    startTime = millis();
+    Serial.println(F("System initialized. Auto-learning will begin in 15 seconds."));
+    Serial.println(F("Position the object to track in front of the camera."));
   }
 }
 
 void loop() {
+  // Check if it's time to auto-learn and if learning hasn't been attempted yet
+  if (!learningAttempted && !huskylens.isLearned() && (millis() - startTime >= AUTO_LEARN_DELAY)) {
+    Serial.println(F("Auto-learning now..."));
+    
+    // Attempt to learn object as ID 1
+    if (huskylens.writeLearn(1)) {
+      Serial.println(F("Object learned successfully as ID 1!"));
+    } else {
+      Serial.println(F("Learning failed. Make sure object is visible to the camera."));
+    }
+    
+    learningAttempted = true; // Mark learning as attempted regardless of success
+    delay(1000); // Short delay to allow HuskyLens to process
+  }
+  
+  // Display countdown if we're waiting to learn
+  if (!learningAttempted && !huskylens.isLearned()) {
+    unsigned long timeRemaining = (startTime + AUTO_LEARN_DELAY) - millis();
+    if (timeRemaining % 1000 < 50) { // Print roughly once per second
+      Serial.print(F("Auto-learning in "));
+      Serial.print(timeRemaining / 1000);
+      Serial.println(F(" seconds..."));
+    }
+  }
+
   if (!huskylens.request()) {
-    Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection."));
+    Serial.println(F("Failed to request data from HUSKYLENS. Check connection."));
   }
-
   else if (!huskylens.isLearned()) {
-    Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one."));
+    // Only show this message if we've already attempted learning
+    if (learningAttempted) {
+      Serial.println(F("No object has been learned. Try repositioning the object."));
+    }
   }
-
   else if (!huskylens.available()) {
-    Serial.println(F("No block or arrow appears on the screen."));
+    Serial.println(F("No object detected on screen."));
   }
-
   else {
-    Serial.println(F("Object Detected."));
+    Serial.println(F("Object detected!"));
     while (huskylens.available()) {
       HUSKYLENSResult result = huskylens.read();
       printResult(result);
       driveBot(result);
     }
   }
+  
+  delay(50); // Short delay to prevent flooding serial
 }
+
 void printResult(HUSKYLENSResult result) {
   if (result.command == COMMAND_RETURN_BLOCK) {
     Serial.print("Block: xCenter=");
@@ -141,6 +166,19 @@ void printResult(HUSKYLENSResult result) {
 }
 
 void go_Advance() {
+  Serial.println("GO FORWARD");
+  set_Motorspeed(SPEED, SPEED);
+
+  digitalWrite(RightDirectPin1, HIGH);
+  digitalWrite(RightDirectPin2, LOW);
+  digitalWrite(LeftDirectPin1, HIGH);
+  digitalWrite(LeftDirectPin2, LOW);
+}
+
+void go_Slow() {
+  Serial.println("SLOW DOWN");
+  set_Motorspeed(SLOW_SPEED1, SLOW_SPEED2);
+
   digitalWrite(RightDirectPin1, HIGH);
   digitalWrite(RightDirectPin2, LOW);
   digitalWrite(LeftDirectPin1, HIGH);
@@ -148,6 +186,9 @@ void go_Advance() {
 }
 
 void go_Left() {
+  Serial.println("TURN LEFT");
+  set_Motorspeed(TURN_SPEED3, TURN_SPEED2);
+
   // digitalWrite(RightDirectPin1, HIGH);
   // digitalWrite(RightDirectPin2,LOW);
   digitalWrite(LeftDirectPin1, LOW);
@@ -155,6 +196,9 @@ void go_Left() {
 }
 
 void go_Right() {
+  Serial.println("TURN RIGHT");
+  set_Motorspeed(TURN_SPEED2, TURN_SPEED1);
+
   digitalWrite(RightDirectPin1, LOW);
   digitalWrite(RightDirectPin2, HIGH);
   // digitalWrite(LeftDirectPin1,HIGH);
@@ -162,6 +206,9 @@ void go_Right() {
 }
 
 void go_Back() {
+  Serial.println("REVERSING");
+  set_Motorspeed(BACK_SPEED1, BACK_SPEED2);
+
   digitalWrite(RightDirectPin1, LOW);
   digitalWrite(RightDirectPin2, HIGH);
   digitalWrite(LeftDirectPin1, LOW);
@@ -169,11 +216,13 @@ void go_Back() {
 }
 
 void stop_Stop() {
+  Serial.println("STOP");
+  set_Motorspeed(0, 0);
+
   digitalWrite(RightDirectPin1, LOW);
   digitalWrite(RightDirectPin2, LOW);
   digitalWrite(LeftDirectPin1, LOW);
   digitalWrite(LeftDirectPin2, LOW);
-  set_Motorspeed(0, 0);
 }
 
 void set_Motorspeed(int speed_L, int speed_R) {
@@ -181,17 +230,15 @@ void set_Motorspeed(int speed_L, int speed_R) {
   analogWrite(speedPinR, speed_R);
 }
 
-void autostopping() {
+void auto_Stopping() {
   int IRvalueLeft = digitalRead(LeftObstacleSensor);
   int IRvalueRight = digitalRead(RightObstacleSensor);
 
   if (IRvalueLeft == LOW || IRvalueRight == LOW) {
-    Serial.println("Infrared Detected Object. STOPPING.");
-    set_Motorspeed(0, 0);
+    Serial.println("Infrared Detected Object.");
     stop_Stop();
-  } else {
-    Serial.println("REVERSING");
-    set_Motorspeed(BACK_SPEED1, BACK_SPEED2);
+  }
+  else {
     go_Back();
   }
 }
@@ -199,23 +246,23 @@ void autostopping() {
 void watch(int &echo_distance_1, int &echo_distance_2) {
   //Sensor 1
   digitalWrite(Trig_PIN_1, LOW);
-  delayMicroseconds(5);            //Wait for the trigger to settle
-  digitalWrite(Trig_PIN_1, HIGH);  //Send the trigger pulse
-  delayMicroseconds(15);           //Pulse length
-  digitalWrite(Trig_PIN_1, LOW);   //Stop the pulse
+  delayMicroseconds(5); //Wait for the trigger to settle
+  digitalWrite(Trig_PIN_1, HIGH); //Send the trigger pulse
+  delayMicroseconds(15); //Pulse length
+  digitalWrite(Trig_PIN_1, LOW); //Stop the pulse
 
-  echo_distance_1 = pulseIn(Echo_PIN_1, HIGH);  //Measure pulse duration
-  echo_distance_1 = echo_distance_1 * 0.01715;  //Convert to distance in cm
+  echo_distance_1 = pulseIn(Echo_PIN_1, HIGH); //Measure pulse duration
+  echo_distance_1 = echo_distance_1 * 0.01715; //Convert to distance in cm
 
   //Sensor 2
   digitalWrite(Trig_PIN_2, LOW);
-  delayMicroseconds(5);            //Wait for the trigger to settle
-  digitalWrite(Trig_PIN_2, HIGH);  //Send the trigger pulse
-  delayMicroseconds(15);           //Pulse length
-  digitalWrite(Trig_PIN_2, LOW);   //Stop the pulse
+  delayMicroseconds(5); //Wait for the trigger to settle
+  digitalWrite(Trig_PIN_2, HIGH); //Send the trigger pulse
+  delayMicroseconds(15); //Pulse length
+  digitalWrite(Trig_PIN_2, LOW); //Stop the pulse
 
-  echo_distance_2 = pulseIn(Echo_PIN_2, HIGH);  //Measure pulse duration
-  echo_distance_2 = echo_distance_2 * 0.01715;  //Convert to distance in cm
+  echo_distance_2 = pulseIn(Echo_PIN_2, HIGH); //Measure pulse duration
+  echo_distance_2 = echo_distance_2 * 0.01715; //Convert to distance in cm
 }
 
 void driveBot(HUSKYLENSResult result) {
@@ -224,43 +271,51 @@ void driveBot(HUSKYLENSResult result) {
 
   if (result.xCenter <= 120) {
     if (echo_distance_1 > distancelimit) {
-      Serial.println("TURN LEFT");
-      set_Motorspeed(TURN_SPEED3, TURN_SPEED2);
       go_Left();
     } else {
-      Serial.println("TURN RIGHT");
-      set_Motorspeed(TURN_SPEED2, TURN_SPEED1);
       go_Right();
     }
   }
-
   else if (result.xCenter >= 180) {
     if (echo_distance_2 > distancelimit) {
-      Serial.println("TURN RIGHT");
-      set_Motorspeed(TURN_SPEED2, TURN_SPEED1);
       go_Right();
     } else {
-      Serial.println("TURN LEFT");
-      set_Motorspeed(TURN_SPEED3, TURN_SPEED2);
       go_Left();
     }
   }
-
   else {
-    if (result.width < 35) {  // Increase threshold to allow forward movement
-      Serial.println("GO FORWARD");
-      set_Motorspeed(SPEED, SPEED);
-      go_Advance();
-    } else if (result.width <= 50) {  // Slow down instead of stopping
-      Serial.println("SLOW DOWN");
-      set_Motorspeed(SLOW_SPEED1, SLOW_SPEED2);
-      go_Advance();
-    } else {
-      Serial.println("STOP");
+    if (result.width < 45) { //Tracked Object's block becomes smaller as it moves forward.
+      if (echo_distance_1 > distancelimit && echo_distance_2 > distancelimit) {
+        go_Advance();
+      }
+      else if (echo_distance_1 < distancelimit && echo_distance_2 < distancelimit){
+        auto_Stopping();
+      }
+      else if (echo_distance_1 < distancelimit) {
+        go_Right();
+      }
+      else if (echo_distance_2 < distancelimit) {
+        go_Left();
+      }
+    }
+    else if (result.width <= 60) { // Slow down instead of stopping 50
+      if (echo_distance_1 > distancelimit && echo_distance_2 > distancelimit) {
+        go_Slow();
+      }
+      else if (echo_distance_1 < distancelimit && echo_distance_2 < distancelimit){
+        auto_Stopping();
+      }
+      else if (echo_distance_1 < distancelimit) {
+        go_Right();
+      }
+      else if (echo_distance_2 < distancelimit) {
+        go_Left();
+      }
+    }
+    else {
       stop_Stop();
       if (result.width > 100) {
-        Serial.println("GO BACK");
-        autostopping();
+        auto_Stopping();
       }
     }
   }
